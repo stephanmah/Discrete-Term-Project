b2ed7d986cc653547af9061fa1c71782b3c1eec8
Stephan Mah <35859912+stephanmah@users.noreply.github.com>
2020-10-23 22:04:10 -0400

    added scanner

--- DSTermProject.java
+++ DSTermProject.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 class DSTermProject {
     public static void main(String[] args) {
-        System.out.println("Test");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a positive number from 1 to 1000");
+        String inputNumber = scan.nextLine();
+        System.out.println("Your number is " + inputNumber);
     }
 }
+
