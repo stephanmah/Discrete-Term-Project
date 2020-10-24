23972af3d264631d326e87cd738cd670fd95ff08
Stephan Mah <35859912+stephanmah@users.noreply.github.com>
2020-10-24 01:38:14 -0400

    Update DSTermProject.py
    random array

--- DSTermProject.py
+++ DSTermProject.py
@@ -1,8 +1,8 @@
 #python instead
+import numpy as np
 print("Enter a positive nonzero integer")
 user_number = input() 
 print(user_number)
-for i in user_number:
-    {
 
-    }
+randnums = np.random.randint(1, 26, user_number)
+print(randnums)
