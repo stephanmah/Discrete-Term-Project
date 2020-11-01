50547b84f7f85406dfd0cd3e5e322ed803db8bd5
Stephan Mah <35859912+stephanmah@users.noreply.github.com>
2020-10-31 20:40:39 -0400

    new files
    progress baby

--- BubbleSortRandomInts.py
+++ BubbleSortRandomInts.py
@@ -1,41 +1,25 @@
-#python instead
 import sys
 import numpy as np
+
+#Takes an integer as input
 print("Enter a positive nonzero integer")
 user_number = int(input())
-print(user_number)
+print("This is array has", user_number, "numbers")
 
 my_generator = np.random.default_rng()
-#randnums = np.random.randint(1, 26, user_number)
 randnums1 = my_generator.integers(0, 100, size = user_number)
-#listed_randnums = randnums.toList()
 print (randnums1)
-#print(type(randnums))
 
 
-#rewrite
 def bubbleSort(randnums):
     n = len(randnums)
-
-    # Traverse through all array elements
     for i in range(n-1):
-        # range(n) also work but outer loop will repeat one time more than needed.
-
-        # Last i elements are already in place
         for j in range(0, n-i-1):
-
-            # traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
             if randnums[j] > randnums[j+1]:
                 randnums[j], randnums[j+1] = randnums[j+1], randnums[j]
 
-
 bubbleSort(randnums1)
 
-print("Bubble Sorted array:")
+print("Bubble sorted array:")
 for i in range(len(randnums1)):
     print("%d" % randnums1[i]),
-
-
-
--- SelectionSortRandomInts.py
+++ SelectionSortRandomInts.py
@@ -1,36 +1,26 @@
-#python instead
 import sys
 import numpy as np
+
+#Takes an integer as input
 print("Enter a positive nonzero integer")
 user_number = int(input())
-print(user_number)
+print("This is array has",user_number,"numbers")
 
 my_generator = np.random.default_rng()
-#randnums = np.random.randint(1, 26, user_number)
 randnums1 = my_generator.integers(0, 100, size=user_number)
-#listed_randnums = randnums.toList()
 print(randnums1)
-#print(type(randnums))
-
-# Traverse through all array elements
 
 
 def selection_sort(randnums):
-    # i indicates how many items were sorted
     for i in range(len(randnums)-1):
-        # To find the minimum value of the unsorted segment
-        # We first assume that the first element is the lowest
-        #
         min_index = i
-        # We then use j to loop through the remaining elements
         for j in range(i+1, len(randnums)-1):
-            # Update the min_index if the element at j is lower than it
             if randnums[j] < randnums[min_index]:
                 min_index = j
-        # After finding the lowest item of the unsorted regions, swap with the first unsorted item
         randnums[i], randnums[min_index] = randnums[min_index], randnums[i]
-# Driver code to test above
+
 selection_sort(randnums1)
+
 print("Selection sorted array")
 for i in range(len(randnums1)):
     print("%d" % randnums1[i]),
